9e8658d7c7aa15b36c37a705cabd4dafed4b175c
Alison Bernardino <58011286+AlisonBernardino@users.noreply.github.com>
2021-03-28 12:40:13 -0300

    Repository root changes

--- tsfiles/index.js
+++
@@ -1,9 +0,0 @@
-var input1 = document.getElementById("n1");
-var input2 = document.getElementById("n2");
-var button = document.getElementById("button");
-function sum(x, y) {
-    return x + y;
-}
-button.addEventListener("click", function () {
-    console.log(umber(input1.value), Number(input2.value));
-});
